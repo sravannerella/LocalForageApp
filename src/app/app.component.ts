e935f15e21a57f51689da0f0c0c5d585fc8f9a69
sravannerella <sravannerella007@hotmail.com>
2018-05-02 17:40:06 -0400

    instances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,110 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import * as localForage from 'localforage';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    myForm: FormGroup;
+    email: FormControl;
+    password: FormControl;
+    dataJSON = {};
+    names: String[] = [];
+
+    userStorage: LocalForage;
+    instances: LocalForage[] = [];
+    data: string[] = [];
+    instanceNum = 0;
+
+    constructor(private toastr: ToastrService) {
+
+        this.instanceNum = this.instanceNum + 1;
+
+        this.userStorage = localForage.createInstance({
+            name: 'localForageDB' + this.instanceNum, // DB NAME
+            storeName: 'logins', // TABLE or collection NAME
+            driver: [
+                localForage.INDEXEDDB,
+                localForage.WEBSQL,
+                localForage.LOCALSTORAGE
+            ]
+        });
+
+        this.instances.push(this.userStorage);
+
+        this.initializeFormControls();
+        this.initForm();
+    }
+
+    initializeFormControls() {
+        this.email = new FormControl('', [Validators.required, Validators.pattern('[^ @]*@[^ @]*')]);
+        this.password = new FormControl('', [Validators.required, Validators.minLength(4)]);
+    }
+
+    initForm() {
+        this.myForm = new FormGroup({
+            email: this.email,
+            password: this.password
+        });
+    }
+
+    dropDB() {
+        const self = this;
+        this.userStorage.clear().then(function() {
+            self.toastr.success('', 'DROP DATABASE');
+        });
+    }
+
+    newInstance() {
+        this.instanceNum++;
+        this.userStorage = localForage.createInstance({
+            name: 'localForageDB' + this.instanceNum,
+            storeName: 'logins',
+            driver: [
+                localForage.INDEXEDDB,
+                localForage.WEBSQL,
+                localForage.LOCALSTORAGE
+            ]
+        });
+        this.instances.push(this.userStorage);
+    }
+
+    showInstances() {
+        this.instances.map( (instance) => {
+            if (this.names.indexOf(instance.config().name) === -1 ) {
+                this.names.push(instance.config().name);
+                instance.getItem('key').then( (data: string) => {
+                    this.data.push(JSON.parse(data));
+                });
+            }
+        });
+    }
+
+    getDataFromDB() {
+        const self = this;
+        this.userStorage.getItem('key').then( (newData) => {
+            self.dataJSON = JSON.parse(JSON.stringify(newData));
+        }, (err) => {
+            console.error('ERROR: ', err);
+        });
+    }
+
+    formSubmit() {
+        if (this.myForm.valid) {
+            console.log('Valid');
+            const data = {'email': this.email.value, 'pass': this.password.value};
+            this.userStorage.setItem('key', JSON.stringify(data));
+            console.log(localForage.driver());
+            this.toastr.success('', 'Submitted');
+        } else {
+            this.toastr.error('', 'Invalid FORM');
+        }
+        this.myForm.reset();
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
+import { ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgForageModule, NgForage, NgForageCache } from 'ngforage';
+import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
@@ -10,9 +13,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    FormsModule,
+    NgForageModule.forRoot()
   ],
-  providers: [],
+  providers: [NgForage, NgForageCache],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
